1e37f122869bf023afaa03c15e2af5382942a963
minnngyu97 <mggkxmjw@gmail.com>
2025-08-14 14:56:00 +0900

    모바일 탭 메뉴 수정

--- js/header.js
+++ js/header.js
@@ -1,17 +1,68 @@
-document.querySelectorAll('.nav-list li').forEach(function(item) {
-  item.addEventListener('mouseenter', function() {
-    let sub = this.querySelector('.sub');
-    if (sub) {
-      sub.classList.add('show');
-    }
-  });
+// document.querySelectorAll('.nav-list li').forEach(function(item) {
+//   item.addEventListener('mouseenter', function() {
+//     let sub = this.querySelector('.sub');
+//     if (sub) {
+//       sub.classList.add('show');
+//     }
+//   });
 
-  item.addEventListener('mouseleave', function() {
-    let sub = this.querySelector('.sub');
-    if (sub) {
-      sub.classList.remove('show');
+//   item.addEventListener('mouseleave', function() {
+//     let sub = this.querySelector('.sub');
+//     if (sub) {
+//       sub.classList.remove('show');
+//     }
+//   });
+// });
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  function initMenuEvents() {
+    const isMobile = window.matchMedia("(max-width: 992px)").matches;
+    const menuItems = document.querySelectorAll('.nav-list > li');
+
+    menuItems.forEach(item => {
+      item.replaceWith(item.cloneNode(true));
+    });
+
+    if (isMobile) {
+      document.querySelectorAll('.nav-list > li').forEach(item => {
+        const subMenu = item.querySelector('.sub');
+        if (subMenu) {
+          item.addEventListener('click', function (e) {
+            e.preventDefault();
+            subMenu.classList.toggle('show');
+          });
+        }
+      });
+
+      const menuBtn = document.querySelector('.menu-btn');
+      const navWrap = document.querySelector('.nav-wrap');
+      if (menuBtn && navWrap) {
+        menuBtn.addEventListener('click', () => {
+          navWrap.classList.toggle('open');
+          document.body.classList.toggle('menu-open');
+        });
+      }
+
+    } else {
+      document.querySelectorAll('.nav-list > li').forEach(item => {
+        const subMenu = item.querySelector('.sub');
+        if (subMenu) {
+          item.addEventListener('mouseenter', () => {
+            subMenu.classList.add('show');
+          });
+          item.addEventListener('mouseleave', () => {
+            subMenu.classList.remove('show');
+          });
+        }
+      });
     }
-  });
+  }
+
+  initMenuEvents();
+
+  window.addEventListener('resize', initMenuEvents);
 });
 
 document.addEventListener('DOMContentLoaded', function () {
